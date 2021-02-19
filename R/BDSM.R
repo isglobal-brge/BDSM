308516e6ae75243fa75ec30fa6cd696ac8b033fd
dpelegri <dpelegri@gmail.com>
2021-02-19 18:06:55 +0100

    First update - BDSM
    Moved all BigDataStatMeth content to new package BDSM

---
+++ R/BDSM.R
@@ -0,0 +1,9 @@
+#' BigDataStatMEth package documentation
+#' @name BDSM
+#' @useDynLib BDSM
+#' @import RcppEigen
+#' @importFrom Rcpp evalCpp
+#' @importFrom Rcpp sourceCpp
+#' @importFrom RcppParallel RcppParallelLibs
+#' @exportPattern("^[[:alpha:]]+")
+NULL
